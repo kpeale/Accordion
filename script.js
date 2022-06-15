05c2f85922610aaa732a3a5a1d78751b07236458
Kpeale Legbara <66691866+kpeale@users.noreply.github.com>
2022-06-15 07:26:13 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const arrow = document.querySelectorAll('.faqsection3img');
+const paragraph = document.querySelectorAll('.faqsection2__div1-a1')
+
+
+const answer =  function () {paragraph.forEach(p => {
+    p.classList.remove('hide')
+})};
+
+const closeArrow = function () {
+    paragraph.classList.add('hide')
+} 
+
+for (let i = 0; i < arrow.length; i++) {
+    arrow[i].addEventListener('click', function(e){
+        this.style.transform = 'scaleY(-1)'
+       answer();
+       if( paragraph.classList.contains ('hide')) {
+           closeArrow();
+       }
+      
+       
+    })
+}
+
+
+
+
+
+
+
+
