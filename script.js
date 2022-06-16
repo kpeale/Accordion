1a9d441d22ee9991e893c58a82a8ee2c49277b12
jomefavourite <jfjomefavourite@gmail.com>
2022-06-16 08:30:36 +0100

    minor updates

--- script.js
+++ script.js
@@ -1,28 +1,20 @@
-'use strict';
+"use strict";
 
-const arrow = document.querySelectorAll('.faqsection3img');
-const paragraph = document.querySelectorAll('.faqsection2__div1-a1')
+const faqHeading = document.querySelectorAll(".faq__heading");
+const paragraph = document.querySelectorAll(".faqsection2__div1-a1");
 
-
-
-const answer =  function () {paragraph.forEach(p => {
-    p.classList.remove('hide')
-})};
-
+const answer = function () {
+  paragraph.forEach((p) => {
+    p.classList.remove("hide");
+  });
+};
 
 const closeArrow = function () {
-    paragraph.classList.add("hide");
-  };
-  
-  for (let i = 0; i < arrow.length; i++) {
-     arrow[i].addEventListener("click", function (e) {
-      paragraph[i].classList.toggle("hide");
-    });
-  }
+  paragraph.classList.add("hide");
+};
 
-
-
-
-
-
-
+for (let i = 0; i < faqHeading.length; i++) {
+  faqHeading[i].addEventListener("click", function (e) {
+    paragraph[i].classList.toggle("hide");
+  });
+}
